93aa22866b9d02c4a22243b94651ddcad22ab80c
Kye W. Shi <shi.kye@gmail.com>
2020-08-29 14:12:50 -0700

    add more integer binary ops

--- src/lib/builtin.ml
+++ src/lib/builtin.ml
@@ -7,18 +7,24 @@ let vals =
     %> Val.Get.Exn.primitive
     %> Val.Primitive.Get.Exn.int
   in
-  [ "int_add",
+  let int_bop name f =
+    name,
     Val.Function
       (fun args ->
          let args = Val.Get.Exn.record args in
          let a = get_int_exn "a" args in
          let b = get_int_exn "b" args in
-         Primitive (Int (Z.add a b))
+         Primitive (Int (f a b))
       ),
     Type.Function
       (Type.Record
          (["a", Type.Primitive (Type.Int);
            "b", Type.Primitive (Type.Int)], None), Type.Primitive (Type.Int))
+  in
+  [ int_bop "int_add" Z.add ;
+    int_bop "int_sub" Z.sub ;
+    int_bop "int_mul" Z.mul ;
+    int_bop "int_div" Z.div ;
   ]
 
 
