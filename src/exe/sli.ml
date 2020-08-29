ed7fa35d9618c07c56f5c95c752b4e8aadeff9db
Kye W. Shi <shi.kye@gmail.com>
2020-08-29 10:30:45 -0700

    add cute [exiting] message on EOF

--- src/exe/sli.ml
+++ src/exe/sli.ml
@@ -12,15 +12,23 @@ let pp =
 
 let () =
   Fmt.(set_style_renderer stdout `Ansi_tty) ;
+  Fmt.(set_style_renderer stderr `Ansi_tty) ;
   while true do
-    let input = LNoise.linenoise "slick> " |> Option.get_exn in
-    LNoise.history_add input |> Result.get_exn ;
-    let expr, _ =
-      Lexing.from_string input
-      |> Slick.Parser.prog Slick.Lexer.read
-      |> Slick.Typing.infer_top Slick.Typing.empty_ctx
-    in
-    pp
-      Fmt.stdout
-      (Slick.Eval.evaluate Slick.Eval.Scope.empty expr, expr.Slick.Ast.Expr.tp)
+    match LNoise.linenoise "slick> " with
+    | None ->
+      let open Fmt in
+      styled (`Fg (`Hi `Magenta)) (any "[exiting]")
+        stderr ()
+    ; exit 0
+
+    | Some input ->
+      LNoise.history_add input |> Result.get_exn ;
+      let expr, _ =
+        Lexing.from_string input
+        |> Slick.Parser.prog Slick.Lexer.read
+        |> Slick.Typing.infer_top Slick.Typing.empty_ctx
+      in
+      pp
+        Fmt.stdout
+        (Slick.Eval.evaluate Slick.Eval.Scope.empty expr, expr.Slick.Ast.Expr.tp)
   done
