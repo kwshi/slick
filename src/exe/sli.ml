9f5188f805ac4462704d6d6b2227fc3a5bc58e48
Kye W. Shi <shi.kye@gmail.com>
2020-08-29 00:37:22 -0700

    auto-format

--- src/exe/sli.ml
+++ src/exe/sli.ml
@@ -3,27 +3,24 @@ open Containers
 let pp =
   let open Fmt in
   hvbox
-    (pair ~sep:(sp ++ styled (`Fg `Blue) (hbox (any ":@ ")))
+    (pair
+       ~sep:(sp ++ styled (`Fg `Blue) (hbox (any ":@ ")))
        (styled (`Fg (`Hi `Cyan)) Slick.Eval.pp)
-       (styled (`Fg (`Hi `Blue)) @@ Slick.Typing.pp)
-    )
+       (styled (`Fg (`Hi `Blue)) @@ Slick.Typing.pp))
   ++ const Format.pp_print_newline ()
 
+
 let () =
-  Fmt.(set_style_renderer stdout `Ansi_tty)
-; while true do
-    let input =
-      LNoise.linenoise "slick> "
-      |> Option.get_exn
-    in
-    LNoise.history_add input
-    |> Result.get_exn
-  ; let expr, _ =
+  Fmt.(set_style_renderer stdout `Ansi_tty) ;
+  while true do
+    let input = LNoise.linenoise "slick> " |> Option.get_exn in
+    LNoise.history_add input |> Result.get_exn ;
+    let expr, _ =
       Lexing.from_string input
       |> Slick.Parser.prog Slick.Lexer.read
       |> Slick.Typing.infer_top Slick.Typing.empty_ctx
     in
-    pp Fmt.stdout
-      (Slick.Eval.evaluate Slick.Eval.Scope.empty expr,
-       expr.Slick.Ast.Expr.tp)
+    pp
+      Fmt.stdout
+      (Slick.Eval.evaluate Slick.Eval.Scope.empty expr, expr.Slick.Ast.Expr.tp)
   done
